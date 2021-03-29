ec2b96c8591d21f22929986a68e8b178612b79db
myprivatealaska <natallia.perats@talentinc.com>
2021-03-28 20:08:33 -0400

    binary_convert.asm first pass

--- 03-intro-to-assembly/binary_convert.asm
+++ 03-intro-to-assembly/binary_convert.asm
@@ -1,4 +1,21 @@
 section .text
 global binary_convert
 binary_convert:
-	ret
+    mov         rcx, 0
+loop:
+    cmp         rdi, 0
+    je          done
+    movzx       rdx, byte[rdi]
+    cmp         rdx, 49
+    je          shift_add_one
+    jne         shift
+    call        loop
+
+shift_add_one:
+    shl         rcx, 1
+    add         rcx, 1
+shift:
+    shl         rcx, 1
+done:
+    mov         rax, rcx
+    ret
