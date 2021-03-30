1a08f89cea8bfab65b9bd178f3210bcc79c29f15
myprivatealaska <natallia.perats@talentinc.com>
2021-03-30 12:09:38 -0400

    binary_convert struggles

--- 03-intro-to-assembly/binary_convert.asm
+++ 03-intro-to-assembly/binary_convert.asm
@@ -2,20 +2,19 @@ section .text
 global binary_convert
 binary_convert:
     mov         rcx, 0
-loop:
-    cmp         rdi, 0
+    mov         rax, 0              ; for total
+    mov         rbx, rdi            ; copy input into rbx
+start_loop:
+    cmp         rdi, 0              ; no need to process if input is empty
     je          done
-    movzx       rdx, byte[rdi]
-    cmp         rdx, 49
-    je          shift_add_one
-    jne         shift
-    call        loop
-
-shift_add_one:
-    shl         rcx, 1
-    add         rcx, 1
-shift:
-    shl         rcx, 1
+    movzx       ecx, byte [rbx]     ; put first byte of rdi into ecx
+    shr         rbx, 8              ; lose the first byte, we only care about subsequent bytes
+    shl         rax, 1              ; total * 2
+    cmp         ecx, 49             ; if current byte is "1", we need to add 1 to total
+    je          add_one
+    jmp         start_loop
+add_one:
+    add         rax, 1
+    jmp         start_loop
 done:
-    mov         rax, rcx
     ret
