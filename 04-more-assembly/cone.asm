70e19ed31de09164438772ef6f8f21cbb4c79c07
myprivatealaska <natallia.perats@talentinc.com>
2021-04-02 17:50:21 -0400

    solve cone

--- 04-more-assembly/cone.asm
+++ 04-more-assembly/cone.asm
@@ -1,6 +1,14 @@
 default rel
 
+section .data
+pi:    dd 3.14159
+three: dd 3.0
+
 section .text
-global volume
+global volume                ; V=pi*r^2*h/3
 volume:
+    mulss   xmm0, xmm0       ; r^2
+    mulss   xmm0, [pi]
+    mulss   xmm0, xmm1
+    divss   xmm0, [three]
  	ret
