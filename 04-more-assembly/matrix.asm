b08b85b5ef44ae9a84363d62d1dc07df35338f64
myprivatealaska <natallia.perats@talentinc.com>
2021-04-02 17:05:15 -0400

    solve matrix

--- 04-more-assembly/matrix.asm
+++ 04-more-assembly/matrix.asm
@@ -6,4 +6,12 @@ index:
 	; rdx: cols
 	; rcx: rindex
 	; r8: cindex
+
+	mov  rbx, 0                 ; clear out rbx
+	imul rcx, rdx               ; the offset = 4 * (rindex * cols + cindex)
+    add  rcx, r8
+    imul rcx, 4
+
+    mov rax, [rdi + rcx]
+
 	ret
