0a376dc733ac6eeb8cb7153a63105a91e86d87e0
myprivatealaska <natallia.perats@talentinc.com>
2021-04-09 16:30:29 -0400

    memory hierarchy - cachegrind results

--- 06-memory-hierarchy/loop-order.c
+++ 06-memory-hierarchy/loop-order.c
@@ -6,29 +6,37 @@ Spotted at:
 http://stackoverflow.com/questions/9936132/why-does-the-order-of-the-loops-affect-performance-when-iterating-over-a-2d-arra
 
 */
+// While both loops execute the same number of instructions, the first loop is ~15 times faster because the second loop
+// jumps around in memory which results in poor spacial locality.
 
-void option_one() {
-  int i, j;
-  static int x[4000][4000];
-  for (i = 0; i < 4000; i++) {
-    for (j = 0; j < 4000; j++) {
-      x[i][j] = i + j;
-    }
-  }
-}
+// D1 (First level data cache) miss rate - 0.9%
+// LL miss rate - 0.3%
 
-//void option_two() {
+//void option_one() {
 //  int i, j;
 //  static int x[4000][4000];
 //  for (i = 0; i < 4000; i++) {
 //    for (j = 0; j < 4000; j++) {
-//      x[j][i] = i + j;
+//      x[i][j] = i + j;
 //    }
 //  }
 //}
 
+
+// D1 (First level data cache) miss rate - 14.3%
+// LL miss rate - 0.9%
+void option_two() {
+  int i, j;
+  static int x[4000][4000];
+  for (i = 0; i < 4000; i++) {
+    for (j = 0; j < 4000; j++) {
+      x[j][i] = i + j;
+    }
+  }
+}
+
 int main() {
-  option_one();
-//  option_two();
+//  option_one();
+  option_two();
   return 0;
 }
